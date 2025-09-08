095a1d73b6caec932363c38b4e8afa52b558b97a
Antonio Estrada <81525959+AntonioTRT@users.noreply.github.com>
2025-09-08 15:51:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#main
+import time
+import utils.angel  # Importa todo el módulo
+
+def main():
+    while True:
+        utils.angel.saludar()  # Llama a la función usando el nombre del módulo
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
